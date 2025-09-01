6d44e60c298a992899a1d53b35e6a2afd3ada789
mbastrykin <maxim@bastrykin.com>
2025-09-01 21:26:17 +0300

    a

---
+++ Enemy.cpp
@@ -0,0 +1,29 @@
+#include "Hero.cpp"
+
+
+
+class Enemy{
+    bool isEnemyShoot = false; bool isEnemyLife;
+
+
+    float complexityEnTime = 5.f; // 5 сек на мин сложности
+    float xE; float yE; // координаты объекта
+
+    Enemy(float x, float y){
+
+        xE = x; yE = y;
+
+    }
+    ~Enemy(){
+
+    }
+
+
+
+    /*void shoot(){
+        if (Время < от сложности и т.д){
+            isEnemyShoot = true;
+        }
+
+    }*/
+};
---
+++ GameLogic.cpp
@@ -0,0 +1,13 @@
+#include "Hero.cpp"
+#include "timer.cpp"
+#include "Level.cpp"
+
+class GameStruct{
+    GameStruct(){
+
+    }
+
+    ~GameStruct(){
+
+    }
+};
